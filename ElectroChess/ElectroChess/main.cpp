bc66702669b01b2e24d79c82db83ea00c2450a40
amt145 <60409801+amt145@users.noreply.github.com>
2020-04-22 23:03:52 -0400

    more polishing

--- ElectroChess/ElectroChess/main.cpp
+++ ElectroChess/ElectroChess/main.cpp
@@ -4,12 +4,11 @@
 	Last Revision Date: 04/24/2020
 */
 
+// Main function of the application
+
 #include "ElectroWin.h"
 #include "Game.h"
-#include <stdio.h>
 int main()
 {
-	//Game* game = new Game(3, 3);
-	//return Game(3, 3).Run();
 	return Game().Run();
 }
